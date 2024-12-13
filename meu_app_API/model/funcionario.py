a0169b8a23e899e64e9703e004300d1594f4fa18
TainaBragante <tainacosta543@gmail.com>
2024-12-12 22:52:17 -0300

    Teste

---
+++ meu_app_API/model/funcionario.py
@@ -0,0 +1,31 @@
+from sqlalchemy import Column, String, Integer, DateTime, Float
+from datetime import datetime
+from typing import Union
+from  model import Base
+
+
+class Funcionario(Base):
+    __tablename__ = 'funcionario'
+
+    id = Column("pk_funcionario", Integer, primary_key=True)
+    nome = Column(String(140), unique=True)
+    venda = Column(Float)
+    porcentagem = Column(Integer)
+    comissao = Column(Float)
+
+
+    def __init__(self, nome:str, porcentagem:int, venda:float, comissao:float):
+        """
+        Cria um Funcionario
+
+        Arguments:
+            nome: nome do funcionario.
+            porcentagem: porcentagem que se espera comprar daquele funcionario
+            venda: venda esperado para o funcionario
+            data_insercao: data de quando o funcionario foi inserido à base
+        """
+        self.nome = nome
+        self.porcentagem = porcentagem
+        self.venda = venda
+        self.comissao = comissao
+
---
+++ meu_app_API/schemas/funcionario.py
@@ -0,0 +1,71 @@
+from pydantic import BaseModel
+from typing import Optional, List
+from model.funcionario import Funcionario
+
+
+class FuncionarioSchema(BaseModel):
+    """ Define como um novo funcionario a ser inserido deve ser representado
+    """
+    nome: str = "Fulano de tal"
+    porcentagem: Optional[int] = 10
+    venda: float = 10000.5
+    #comissao: float = 1000.05
+
+class FuncionarioBuscaSchema(BaseModel):
+    """ Define como deve ser a estrutura que representa a busca. Que será
+        feita apenas com base no nome do funcionario.
+    """
+    nome: str = "Teste"
+
+
+class ListagemFuncionariosSchema(BaseModel):
+    """ Define como uma listagem de funcionarios será retornada.
+    """
+    funcionarios:List[FuncionarioSchema]
+
+
+class FuncionarioViewSchema(BaseModel):
+    """ Define como um funcionario será retornado: funcionario.
+    """
+    id: int = 1
+    nome: str = "Fulano de tal"
+    porcentagem: Optional[int] = 10
+    venda: float = 10000.5
+    comissao: float = 1000.05
+
+def apresenta_funcionarios(funcionarios: List[Funcionario]):
+    """ Retorna uma representação do funcionario seguindo o schema definido em
+        FuncionarioViewSchema.
+    """
+    result = []
+    for funcionario in funcionarios:
+        result.append({
+            "nome": funcionario.nome,
+            "porcentagem": funcionario.porcentagem,
+            "venda": funcionario.venda,
+            "comissao": funcionario.comissao,
+        })
+
+    return {"funcionarios": result}
+
+
+def apresenta_funcionario(funcionario: Funcionario):
+    """ Retorna uma representação do funcionario seguindo o schema definido em
+        FuncionarioViewSchema.
+    """
+    return {
+        "id": funcionario.id,
+        "nome": funcionario.nome,
+        "porcentagem": funcionario.porcentagem,
+        "venda": funcionario.venda,
+        "comissao": funcionario.comissao
+    
+    }
+
+
+class FuncionarioDelSchema(BaseModel):
+    """ Define como deve ser a estrutura do dado retornado após uma requisição
+        de remoção.
+    """
+    mesage: str
+    nome: str
